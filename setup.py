5da7a3b39082b59845540e1399a64b00adcadf4a
tarkus <tarkus@kollemate.iwr.uni-heidelberg.de>
2023-02-10 16:33:31 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='competence estimation',
+    version='1.0',
+    description='A module to increase the reliability of classifications',
+    author='Jens Mueller',
+    author_email='jens.mueller@iwr.uni-heidelberg.de',
+    packages=find_packages(),
+    install_requires=[], #external packages as dependencies                                                                                                    
+)
